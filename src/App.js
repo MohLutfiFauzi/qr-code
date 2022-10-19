61611cd1d11720d58e4e7bbfd7f73a639144b501
mlfauzi54@gmail.com <mlfauzi54@gmail.com>
2022-10-19 21:25:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import imageQrCode from './image/image-qr-code.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='qr-code-card'>
+        <img src={imageQrCode} alt='qr code' />
+        <div className='qr-description'>
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
     </div>
   );
 }
